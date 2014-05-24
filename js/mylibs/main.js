483c759ba96d0a62f7e3a5a99ed5c29fa08c652e
marmuel <mmoogles@googlemail.com>
2014-05-24 08:03:21 +0200

    -performance optimization in the calculation -discount calculation

--- js/mylibs/main.js
+++ js/mylibs/main.js
@@ -1,5 +1,5 @@
 // Register Angular Translate & Bootstrap UI
-var app = angular.module("lexoffice",['pascalprecht.translate', 'ui.bootstrap']);
+var app = angular.module("lexoffice", ['pascalprecht.translate', 'ui.bootstrap']);
 
 //Translation
 app.config(['$translateProvider',
@@ -15,39 +15,52 @@ function($translateProvider) {
 
 	// Tell the module what language to use by default
 	$translateProvider.preferredLanguage('en_US');
-	
-	
-//START THE MAIN CONTROLLER
-}]).controller('CtrlInvoice', ['$scope', '$translate', '$modal', '$window', function($scope, $translate, $modal, $window) {
+
+	//START THE MAIN CONTROLLER
+}]).controller('CtrlInvoice', ['$scope', '$translate', '$modal', '$window',
+function($scope, $translate, $modal, $window) {
 
 	$scope.setLang = function(langKey) {
 		// You can change the language during runtime
 		$translate.use(langKey);
 	};
 
-//Invoice Control
+	//Invoice Control
 
 	$scope.class = "glyphicon glyphicon-minus";
 	$scope.logoRemoved = false;
 	$scope.printMode = false;
 
-  var sample_invoice = {
-  	        invoice_number: 1000,
-              items:[ {qty:10, taxOne: '', taxTwo: '', description:'Tablet', cost:99.95}]};
+	var sample_invoice = {
+		invoice_number : 1000,
+		discount : 0,
+		items : [{
+			qty : 10,
+			taxOne : '',
+			taxTwo : '',
+			description : 'Tablet',
+			cost : 99.95
+		}]
+	};
 
-    if(localStorage["lexoffice"] == "" || localStorage["lexoffice"] == null){
-  	console.log('Sample Invoice');
-    $scope.invoice = sample_invoice;
-  }
-  else{
-  	console.log('Stored Invoice');
-    $scope.invoice =  JSON.parse(localStorage["lexoffice"]);
-  }
-    $scope.addItem = function() {
-        $scope.invoice.items.push({description:"", qty:0, cost:0, taxOne:'', taxTwo:''});    
-    };
+	if (localStorage["lexoffice"] == "" || localStorage["lexoffice"] == null) {
+		console.log('Sample Invoice');
+		$scope.invoice = sample_invoice;
+	} else {
+		console.log('Stored Invoice');
+		$scope.invoice = JSON.parse(localStorage["lexoffice"]);
+	}
+	$scope.addItem = function() {
+		$scope.invoice.items.push({
+			description : "",
+			qty : 0,
+			cost : 0,
+			taxOne : '',
+			taxTwo : ''
+		});
+	};
 
-   $scope.removeLogo = function(element) {
+	$scope.removeLogo = function(element) {
 
 		if ($scope.class === "glyphicon glyphicon-plus") {
 			$scope.class = "glyphicon glyphicon-minus";
@@ -67,24 +80,22 @@ function($translateProvider) {
 		$scope.logoRemoved = false;
 	};
 
-
 	//show and hide shipping costs //
-	
+
 	// set default to no
-	
+
 	$scope.radioModel = 'No';
-  $scope.checkModel = {
-    yes: false,
-    no: true
-  };
+	$scope.checkModel = {
+		yes : false,
+		no : true
+	};
 
 	$scope.shipping = {
 		fields : [{
 			shippingcosts : 0.00,
 			isRowHidden : true
 		}]
-		
-		
+
 	};
 
 	$scope.hideShippingCosts = function(field) {
@@ -100,184 +111,186 @@ function($translateProvider) {
 		$scope.invoice.items.splice($scope.invoice.items.indexOf(item), 1);
 	};
 
+	// Callculate Tax and dynamically add new rows for subtotals
 
-	// Calculations (Taxes are calculated directly in the view)
-	
-	// SubTotal
-	$scope.invoice_sub_total = function() {
-		var total = 0.00;
-		angular.forEach($scope.invoice.items, function(item, key) {
-			total += (item.qty * item.cost);
+	$scope.grouppedByPercentage = function() {
+		var groups = {};
+		$scope.invoice.items.forEach(function(invoice) {
+			['taxOne', 'taxTwo'].forEach(function(key) {
+				var perc = invoice[key];
+				if (perc === '0' || perc === "") {
+					return;
+				}// ignore 0 percentage
+
+				if (!groups[perc]) {
+					groups[perc] = 0;
+				}
+
+				groups[perc] += invoice.cost * invoice.qty;
+			});
 		});
-		return total;
-	};
-	
-	// Discount TODO Angular combined with jQuery..quick & dirty
-	$scope.invoice_discount = function() {
-		 // check if discount-input is empty
-		 var disCount = $('#discount').val();
-		if (disCount === '') {
-				    return;
-	  } else { 
-	  			return (($scope.invoice_sub_total()  * $scope.discount.show)/100); 
-		}
- };
- 
- // TODO Shipping Costs and Taxes!
-	// Grand Total / Balance
-	$scope.calculate_grand_total = function() {
-		localStorage["lexoffice"] = JSON.stringify($scope.invoice);
-		var disCount = $('#discount').val();
-		// check if discount-input is empty
-		if (disCount === '') {
-			return $scope.invoice_sub_total();
-	  } else {
-	  		return $scope.invoice_sub_total() - $scope.invoice_discount();
-		}
-			
+		return groups;
 	};
 
+	//---------- For alternative approach ----------\\
+	$scope.$watch('invoice', function(newValue, oldValue) {
+		// Arr for multiple Taxes
+		$scope.groupsArr = convertToArray($scope.grouppedByPercentage());
+
+		// Calculations (Taxes are calculated directly in the view)
+
+		// SubTotal
+		$scope.invoice_sub_total = function() {
+			var total = 0.00;
+			angular.forEach($scope.invoice.items, function(item, key) {
+				total += (item.qty * item.cost);
+			});
+			return total;
+		};
+		// Discount
+
+		$scope.invoice_discount = function() {
+
+			disCount = $scope.invoice.discount;
+			if (disCount == '' || disCount == 'NaN') {
+				return;
+			} else {
+				return (($scope.invoice_sub_total() * disCount) / 100);
+			}
+		};
+
+		// Grand Total / Balance
+		$scope.calculate_grand_total = function() {
+			localStorage["lexoffice"] = JSON.stringify($scope.invoice);
+			//var disCount = parseFloat($scope.discount);
+			disCount = $scope.invoice.discount;
+			// check if discount-input is empty
+			if (disCount === '' || disCount == '0') {
+				console.log('kein Discount');
+				return $scope.invoice_sub_total();
+			} else {
+				console.log('mit Discount');
+				return $scope.invoice_sub_total() - (($scope.invoice_sub_total() * disCount) / 100);
+				
+			}
+
+		};
+
+	}, true);
+
+	function convertToArray(groups) {
+		var arr = [];
+		angular.forEach(groups, function(value, key) {
+			arr.push({
+				perc : key,
+				sum : value
+			});
+		});
+		return arr;
+	}
 
 
 	$scope.printInfo = function() {
 		window.print();
 	};
-	
-// Callculate Tax and dynamically add new rows for subtotals
- 
-    $scope.grouppedByPercentage = function () {
-        var groups = {};
-        $scope.invoice.items.forEach(function (invoice) {
-            ['taxOne', 'taxTwo'].forEach(function (key) {
-                var perc = invoice[key];
-                if (perc === '0' || perc === "") { return; }   // ignore 0 percentage
 
-                if (!groups[perc]) {
-                    groups[perc] = 0;
-                }
-                
-                groups[perc] += invoice.cost * invoice.qty;
-            });
-        });
-        return groups;
-    };
+	// Modal Dialog Email
 
-//---------- For alternative approach ----------\\    
-    $scope.$watch('invoice', function (newValue, oldValue) {
-        $scope.groupsArr = convertToArray($scope.grouppedByPercentage());
-    }, true);
-
-    function convertToArray(groups) {
-        var arr = [];
-        angular.forEach(groups, function (value, key) {
-            arr.push({perc: key, sum: value});
-        });
-        return arr;
-    }
-
-	
-// Modal Dialog Email
-	
-	$scope.openModalEmail = function (size) {
-    var modalInstance = $modal.open({
-      templateUrl: 'EmailModalContent.html',
-      controller: ModalInstanceCtrl,
-      size: size,
-      resolve: {
-        items: function () {
-          return $scope.items;
-        }
-      }
-    });
-  };
-  // Modal Dialog Reset
-  
-  	$scope.openModalReset = function (size) {
-    var modalInstance = $modal.open({
-      templateUrl: 'ResetModalContent.html',
-      controller: ModalInstanceCtrl,
-      size: size,
-      resolve: {
-        items: function () {
-          return $scope.items;
-        }
-      }
-    });
-  };
-  
-// Please note that $modalInstance represents a modal window (instance) dependency.
-// It is not the same as the $modal service used above.
-
-var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
-// Email
-  $scope.ok = function () {
-    $modalInstance.dismiss('cancel');
-    //do some stuff and send email after o.k. 
-  };
-  
-    $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-  
-  
-  $scope.resetStorage = function () {
-    $modalInstance.dismiss('cancel');
-		 
-			localStorage["lexoffice"] = "";
-			$scope.invoice = sample_invoice;	
-
+	$scope.openModalEmail = function(size) {
+		var modalInstance = $modal.open({
+			templateUrl : 'EmailModalContent.html',
+			controller : ModalInstanceCtrl,
+			size : size,
+			resolve : {
+				items : function() {
+					return $scope.items;
+				}
+			}
+		});
 	};
- }; 
+	// Modal Dialog Reset
+
+	$scope.openModalReset = function(size) {
+		var modalInstance = $modal.open({
+			templateUrl : 'ResetModalContent.html',
+			controller : ModalInstanceCtrl,
+			size : size,
+			resolve : {
+				items : function() {
+					return $scope.items;
+				}
+			}
+		});
+	};
+
+	// Please note that $modalInstance represents a modal window (instance) dependency.
+	// It is not the same as the $modal service used above.
+
+	var ModalInstanceCtrl = function($scope, $modalInstance, items) {
+		// Email
+		$scope.ok = function() {
+			$modalInstance.dismiss('cancel');
+			//do some stuff and send email after o.k.
+		};
+
+		$scope.cancel = function() {
+			$modalInstance.dismiss('cancel');
+		};
+
+		$scope.resetStorage = function() {
+			$modalInstance.dismiss('cancel');
+
+			localStorage["lexoffice"] = "";
+			$scope.invoice = sample_invoice;
+
+		};
+	};
 }])
-
-
 
 //coma dot converter (custom directive)
 
-.directive("comaDotConverter",function(){
-   return {
-            require: 'ngModel',
-            link: function (scope, element, attrs, modelCtrl) {
-              
-                modelCtrl.$parsers.push(function(inputValue) {
-                    
-                    if (typeof (inputValue) == "undefined") return '';
-                    var transformedInput = inputValue.replace(/,/g,'.');
-                    
-                    if (transformedInput != inputValue) {
-                        modelCtrl.$setViewValue(transformedInput);
-                        modelCtrl.$render();
-                    }
+.directive("comaDotConverter", function() {
+	return {
+		require : 'ngModel',
+		link : function(scope, element, attrs, modelCtrl) {
 
-                    return transformedInput;
-                });
-            }
-        };
-  
+			modelCtrl.$parsers.push(function(inputValue) {
+
+				if ( typeof (inputValue) == "undefined")
+					return '';
+				var transformedInput = inputValue.replace(/,/g, '.');
+
+				if (transformedInput != inputValue) {
+					modelCtrl.$setViewValue(transformedInput);
+					modelCtrl.$render();
+				}
+
+				return transformedInput;
+			});
+		}
+	};
+
 })
-
 // Animation for e.g. Row add / delete
 
-.directive('jqAnimate', function(){ 
-  return function(scope, instanceElement){ 
-      setTimeout(function() {instanceElement.show('slow');}, 0); 
-  };
+.directive('jqAnimate', function() {
+	return function(scope, instanceElement) {
+		setTimeout(function() {
+			instanceElement.show('slow');
+		}, 0);
+	};
 });
 // ACHTUNG WENN EINE WEITERE DIREKTIVE HINZUKOMMT ; semicolon ENTFERNEN!!!!!
 
-
-
 function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            $('#company_logo').attr('src', e.target.result);
-        }
-        reader.readAsDataURL(input.files[0]);
-    }
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			$('#company_logo').attr('src', e.target.result);
+		};
+		reader.readAsDataURL(input.files[0]);
+	}
 }
-
-
 
 // window.onbeforeunload = function(e) {
 //   confirm('Are you sure you would like to close this tab? All your data will be lost');
@@ -286,11 +299,10 @@ function readURL(input) {
 $(document).ready(function() {
 	//set default currency
 	$("#currency").val('USD');
-	
+
 	$("#currency").trigger('change');
 	$("#logoCompany").change(function() {
 		readURL(this);
 	});
 });
-
 
